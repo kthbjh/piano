43d81876504d9d9950c9e189635e14babee7de05
KimK4N1 <01041301061a@gmail.com>
2022-08-10 22:32:45 +0900

    난이도 변경부분만 함

--- piano.ino
+++ piano.ino
@@ -1,9 +1,21 @@
 
 const int buzzerPin = A4;
+int dfcRLED = A5;
+int dfcGLED = A6;
+int dfcBLED = A7;
 const int hz[11] = {262, 277, 294, 311, 330, 349, 370, 415, 440, 466, 494};
+const int dfcButton = 5;
+const int startButton = 6;
+int difficult = 1;
 
 void setup() {
     pinMode(buzzerPin, OUTPUT);
+    pinMode(dfcRLED, OUTPUT);
+    pinMode(dfcGLED, OUTPUT);
+    pinMode(dfcBLED, OUTPUT);
+
+    pinMode(dfcButton, INPUT);
+    pinMode(startButton, INPUT);
 }
 
 void loop() {
@@ -14,9 +26,28 @@ void loop() {
     }
     // delay(500);
     noTone(buzzerPin);
-    delay(500000);
+    delay(1000);
 }
 
-void difficultCho(){
+bool prebutton = false;
+void dfcCho() {
     if 
+}
+
+int dfcChg() {
+    difficult = (difficult + 1) % 3;
+    if (difficult == 0) {
+        analogWrite(dfcRLED, 0);
+        analogWrite(dfcGLED, 255);
+    }
+    else if (difficult == 1) {
+        analogWrite(dfcRLED, 255);
+        analogWrite(dfcGLED, 190);
+    }
+    else {
+        analogWrite(dfcRLED, 255);
+        analogWrite(dfcGLED, 0);
+    }
+    analogWrite(dfcBLED, 0);
+
 }
