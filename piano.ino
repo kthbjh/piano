e7b1354e16eef11d91ff65d3939f3fc45695bf21
csnewcs <cs.newcs0@gmail.com>
2022-08-31 20:43:31 +0900

    8-31 remake
    Co-authored-by: KimK4N1 <KimK4N1@users.noreply.github.com>

--- piano.ino
+++ piano.ino
@@ -1,124 +1,41 @@
-int difficult = 0;
-bool gamingTF = false;
-int dfcLED[3] = {A0, A1, A2}; // RGB
-int buzzerPin[3] = {A3, A4, A5}; 
-const int sclBtn[8] = {
-    A6, A7, A8, A9, A10, A11, A12, A13
-}; 
-const int dfcButton = 5;
-const int startButton = 6;
-const int answerLED[6] = {22, 23, 24, 25, 26, 27};
+#include "Polytone.h"
 
-int hz[8] = {
-    262, 294, 330, 349, 392, 440, 494, 523
-};// C,   D,   E,   F,   G,   A,   B,   HC
-bool settingButtonPushed[2] = {
-    false, false
-};
-bool pianoButtonPushed[8] = {
-    false, false, false, false, false, false, false, false
-};
+Polytone poly;
 
+const int buzzerPin[3] = {10, 11, 12};
+const int hz[8] = {262, 294, 330, 349, 392, 440, 494, 523};
 
+enum Difficulty {
+    EASY,
+    NORMAL,
+    HARD
+}
+// Difficulty gameDifficulty = EASY;
+
+void changeDifficulty() {
+    gameDifficulty = gameDifficulty + 1;
+    
+}
+void startGame() {
+
+}
+void makeRandomTones() {
+
+}
+void playRandomTones() {
+
+}
+void answerEntered() {
+
+}
+bool isCorrected() {
+
+}
+void gameFinished() {
+
+}
 
 void setup() {
-    Serial.begin(9600);
-    pinMode(buzzerPin[0], OUTPUT);
-    pinMode(buzzerPin[1], OUTPUT);
-    pinMode(buzzerPin[2], OUTPUT);
-    pinMode(dfcLED[0], OUTPUT);
-    pinMode(dfcLED[1], OUTPUT);
-    pinMode(dfcLED[2], OUTPUT);
-    for (int i=0; i<6; i++){
-        pinMode(answerLED[i], OUTPUT);
-    }
-
-    pinMode(dfcButton, INPUT);
-    pinMode(startButton, INPUT);
-    for (int i=0;i<8;i++){
-        pinMode(sclBtn[i], INPUT);
-    }
-    analogWrite(dfcLED[0], 0);
-    analogWrite(dfcLED[1], 255);
-}
-
-// void btnpushcheck() {
-//     for (int i = 0; i < 8; i++){
-//         if (analogRead(sclBtn[i]) > 1000) {
-//             buttonPush[i] = true;
-//             return;
-//         }
-//     }
-// }
-
-
-// void dfcChoice() {
-//     if (){
-//     }
-// }
-
-void dfcChange() {
-    difficult = (difficult + 1) % 3;
-    if (difficult == 0) {
-        analogWrite(dfcLED[0], 0);
-        analogWrite(dfcLED[1], 255);
-    }
-    else if (difficult == 1) {
-        analogWrite(dfcLED[0], 255);
-        analogWrite(dfcLED[1], 230);
-    }
-    else {
-        analogWrite(dfcLED[0], 255);
-        analogWrite(dfcLED[1], 0);
-    }
-    analogWrite(dfcLED[2], 0);
-
-}
-
-int gameTone[3] = {-1, -1, -1};
-void dfcCheck() {
-    if (digitalRead(startButton) == true) {
-        gameStart();
-    }
-}
-
-void randomArray(int length,int equalmore,int less) {
-    //배열(랜덤으로 얻을 수 있는 값이 다 들어있는), 그 배열을 섞어, 그 배열의 0번~n번까지만
-    for (int i = 0 ; i<length ; i++) {
-        gameTone[i] = random(equalmore, less);
-    }
-}
-void gameStart() {
-    char text[100];
-    Serial.println(difficult);
-    if (difficult == 0) {
-        randomArray(1, 0, 8);
-        tone(buzzerPin[0], hz[gameTone[0]], 1500);
-        Serial.println("EASY");
-        snprintf(text, 100, "hz is %i", hz[gameTone[0]]);
-    }
-    else if (difficult == 1) {
-        randomArray(2, 0, 8);
-        for (int i = 0; i<2 ; i++){
-            tone(buzzerPin[i], hz[gameTone[i]]);
-        }
-        delay(1000);
-        for (int i = 0; i<2 ; i++){
-            noTone(buzzerPin[i]);
-        }
-        Serial.println("NORMAL");
-        snprintf(text, 100, "hz are %i and %i", hz[gameTone[0]], hz[gameTone[1]]);
-    }
-    else {
-        randomArray(3, 0, 8);
-        for (int i = 0; i<3 ; i++){
-            tone(buzzerPin[i], hz[gameTone[i]], 1500);
-        }
-        Serial.println("DIFFICULT");
-        snprintf(text, 100, "hz are %i %i and %i", hz[gameTone[0]], hz[gameTone[1]], hz[gameTone[2]]);
-    }
-    Serial.println(text);
-    gamingTF = true;
 }
 
 int pushedBtns[3] = {0, 0, 0};
@@ -139,29 +56,9 @@ void answerCheck() {
     }
 }
 void loop() {
-    if(!gamingTF) {
-        if(digitalRead(dfcButton) && !settingButtonPushed[0]) {
-            dfcChange();
-            settingButtonPushed[0] = true;
-        }
-        else settingButtonPushed[0] = false;
-        if(digitalRead(startButton) && !settingButtonPushed[1]) {
-            gameStart();
-            settingButtonPushed[1] = true;
-        }
-        else settingButtonPushed[1] = false;
-    }
-    delay(100);
-
-    // btnpushcheck();
-    // if (!gamingTF) {
-    //     dfcChoice();
-    //     dfcCheck();
-    //     return;
-    // }
-    // else {
-
-    // }
-
-    // delay(1000);
+    
 }
+
+// void difficultCho(){
+//     if 
+// }
